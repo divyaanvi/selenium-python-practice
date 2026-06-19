ef585edcb290441f3e45c6a1c7dc6846ba126291
Divyashree <divyashree.tk@kyndryl.com>
2026-06-19 14:07:09 +0530

    Added project documentation

---
+++ tests/verify_multipleproducts.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
+
+cart_count = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+
+if cart_count=="2":
+    print("cart count matches")
+else:
+    print("cart count doesnt match")
+
+driver.quit()
